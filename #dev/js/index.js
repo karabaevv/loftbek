898e4d1cb6b24effbc5379fa61ea4fb916c4b6cc
Firdavs Jorullayev <jorullaev61@gmail.com>
2025-12-13 13:27:58 +0500

    js

--- #dev/js/index.js
+++ #dev/js/index.js
@@ -1,1 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {})
+document.addEventListener("DOMContentLoaded", () => {
+  const heroSwiper = new Swiper(".mySwiper", {
+    loop: true,
+    speed: 500,
+    effect: "fade",
+    autoplay: { delay: 4000 },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  const swiper = new Swiper(".popularSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 32,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  const recommendSwiper = new Swiper(".recommendSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 32,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
